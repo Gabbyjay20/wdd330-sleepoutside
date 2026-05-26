a2cb04f000a6ce8fb7c8548dc83224a9b4b9b21f
Gabriel James <gabbyjay20@gmai.com>
2026-05-26 16:55:38 +0100

    Add product detail page to Vite build

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -3,16 +3,19 @@ const baseURL = import.meta.env.VITE_SERVER_URL;
 function convertToJson(res) {
   if (res.ok) {
     return res.json();
-  } else {
-    throw new Error('Bad Response');
   }
+
+  throw new Error('Bad Response');
 }
 
 export default class ProductData {
   constructor() {}
 
   async getData(category) {
-    const response = await fetch(`${baseURL}products/search/${category}`);
+    const response = await fetch(
+      `${baseURL}products/search/${category}`,
+    );
+
     const data = await convertToJson(response);
 
     return data.Result;
@@ -20,7 +23,10 @@ export default class ProductData {
 
   async findProductById(id) {
     const response = await fetch(`${baseURL}product/${id}`);
+
     const data = await convertToJson(response);
+
+    console.log('PRODUCT DETAIL DATA:', data);
 
     return data.Result || data;
   }
